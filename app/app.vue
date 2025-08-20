d38f570e6bb2118962e7f0354c7d615b87f13ce2
FurWaz <fur.waz06@gmail.com>
2025-08-20 21:33:34 +0200

    printing out env variable to test

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,8 @@
     <audio id="audio" src="/music/main.mp3" autoplay loop></audio>
   </div>
 </template>
+
+<script lang="ts" setup>
+
+console.log('NUXT_APP_BASE_URL = ', import.meta.env.NUXT_APP_BASE_URL);
+</script>
